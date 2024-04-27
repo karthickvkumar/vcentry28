95e272c725de6c94b49c33c2938496502d4adb77
kvelraj_concord <kvelraj@concord.net>
2024-04-27 13:00:45 +0530

    commit message

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <div>
+      <h1>Welcome to React JS</h1>
+      <h2>Hello</h2>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+/*
+React - Function Component Syntax
+import React from "react";
+
+const ComponentName = () => {
+  return(
+    HTML Code 
+  )
+}
+
+export default ComponentName;
+*/
+/*
+Naming Convention 
+1. Pascel Case - KarthickKumar (Component or class name)
+2. Camel Case - karthickKumar (Variable name or functipn namee)
+3. Snake Case - karthick_kumar (Css classes)
+4. Kebab Case - karthick-kumar (Css classes)
+*/
