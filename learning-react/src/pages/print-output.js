000b88980c5f2350777e643afc023ab048c3b128
kvelraj_concord <kvelraj@concord.net>
2024-05-12 12:06:05 +0530

    commit message

--- learning-react/src/pages/print-output.js
+++ learning-react/src/pages/print-output.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const PrintOutputPage = () => {
 
-  var username = "karthickkumar";
+  // let [variableName, functionName] = useState(initailData);
+  let [contentMessage, updateContentMessage] = useState("Hello useState!");
+
+  var username = "karthickkumar";  // Function Scope (Local scope)
+  let name = ""; // Block level scope (Local scope)
+  const age = 10;
   // Interpolation {variableName}   -- Primitive type
 
   var userDetails = {
@@ -30,10 +35,51 @@ const PrintOutputPage = () => {
     return(
       <li key={index}>User Full name is {value.firstName} {value.lastName}</li>
     )
+  });
+
+  /*
+  Callback Function  () => {}
+  variableName.map((value, index) => {
+
   })
-
+ */
   console.log(finalResult);
+  
+  function displays(){
+    // ES5 Function Syntax
+    //Function Level Scope
+    if(true){
+      //Block Level Scope
+      var userOld = "ES5";
+      let userNew = "ES6";   // can reassign value
+      const userConst = "ES100";   // cannot reassign value
+      console.log(userNew); 
+      console.log(userConst);  
+   
+      //Edit
+      userOld = "0000";
+      userNew = "1111"; 
+      userConst = "2222";
+      console.log(userNew); 
+    }
+     console.log(userOld);
+  }
 
+  let userMessage = "Hello";
+
+  const display = () => {
+    // ES6 Function Syntax
+    const message = "Hello! Good Day";
+    alert(message);
+  }
+
+  const getInputValue = (event) => {
+    // console.log(event.target.value);
+    userMessage = event.target.value;
+    console.log(userMessage);
+  }
+
+  
   return (
     <div>
       <h1>Printing a Variable value into HTML</h1>
@@ -43,6 +89,13 @@ const PrintOutputPage = () => {
       <ol>
         {finalResult}
       </ol>
+
+      <button onClick={() => display()}>Display</button>
+      <br/>
+
+      <input type="text" placeholder="Enter any message" onChange={getInputValue} />
+      <h2>{userMessage}</h2>
+      <h2>{contentMessage}</h2>
     </div>
   );
 };
