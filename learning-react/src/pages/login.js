562d4c07aca26fcef6fe16ac064590a673ae558c
kvelraj_concord <kvelraj@concord.net>
2024-05-25 11:40:25 +0530

    commit message

--- learning-react/src/pages/login.js
+++ learning-react/src/pages/login.js
@@ -2,39 +2,67 @@ import React, {useState} from "react";
 
 const LoginPage = () => {
 
-  let [username, updateUserName] = useState("");
-  let [password, updatePassword] = useState("");
+  // let name = "mr.abc";
+  let [name, updateName] = useState("mr.abc");
 
-  const handleUsername = (event) => {
-    updateUserName(event.target.value);
+  // let [username, updateUserName] = useState("");
+  // let [password, updatePassword] = useState("");
+
+  let [loginForm, updateLoginForm] = useState({
+    username : "",
+    password: ""
+  })
+
+  // const handleUsername = (event) => {
+  //   console.log(event.target.value);
+
+  //   updateUserName(event.target.value);
+  // }
+
+  // const handlePassword = (event) => {
+  //   updatePassword(event.target.value);
+  // }
+
+  const handleInputField = (event) => {
+    console.log(event.target.value, event.target.name);
+
+    updateLoginForm({...loginForm, [event.target.name] : event.target.value});
   }
 
-  const handlePassword = (event) => {
-    updatePassword(event.target.value);
-  }
 
   const onLogin = () => {
-    let data = {
-      username : username,
-      password : password
-    }
+    // let data = {
+    //   username : "",
+    //   password : ""
+    // }
 
-    console.log(data);
+    // console.log(data);
+
+    console.log(loginForm);
+  }
+
+  const rePrint = () => {
+    //name = "karthi";
+    updateName("karthi");
   }
 
   return(
     <div>
       <h2 id="loginHeading">Login Form</h2>
       <label>Enter your Username</label>
-      <input type="text" placeholder="Please enter username" onChange={handleUsername} />
+      <input type="text" name="username" placeholder="Please enter username" onChange={handleInputField} />
       <br></br>
       <label>Enter your Password</label>
-      <input type="password" placeholder="Please enter password" onChange={handlePassword} />
+      <input type="password" name="password" placeholder="Please enter password" onChange={handleInputField} />
       <br></br>
       <button onClick={() => onLogin()}>Login</button>
 
-      <h2>Your username is {username}</h2>
-      <h2>Your password is {password}</h2>
+      {/* <h2>Your username is {username}</h2>
+      <h2>Your password is {password}</h2> */}
+      <h2>Your username is {loginForm.username}</h2>
+      <h2>Your password is {loginForm.password}</h2>
+      <h1>{name}</h1>
+      <button onClick={() => rePrint()}>Click</button>
     </div>
   )
 }
