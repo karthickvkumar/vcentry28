6a0d13927ecc46c0901f1b0bf22e1bf4e0014961
kvelraj_concord <kvelraj@concord.net>
2024-06-01 12:41:34 +0530

    commit message

--- learning-react/src/pages/api-page.js
+++ learning-react/src/pages/api-page.js
@@ -1,44 +1,67 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const ApiPage = () => {
 
   let [userList, updateUserList] = useState([]);
+  let [userForm, updateUserForm] = useState({
+    name : "",
+    job : ""
+  })
 
   const getAPIMethod = () => {
 
     const URL = "https://reqres.in/api/users?page=2";
 
     axios.get(URL)
-      .then( (response) => {
+      .then((response) => {
         console.log(response.data.data);
 
         updateUserList(response.data.data);
       })
-      .catch( (error) => {
+      .catch((error) => {
         console.log(error);
         alert(error.message);
       })
 
-   /* axios.get(url)
-      .then(() => {})
-      .catch(() => {}) */
-   
+    /* axios.get(url)
+       .then(() => {})
+       .catch(() => {}) */
+
   }
 
   let usersResult = userList.map((value, index) => {
-    return(
+    return (
       <tr key={index}>
         <td>{value.id}</td>
         <td>{value.first_name}</td>
         <td>{value.last_name}</td>
         <td>{value.email}</td>
         <td>
-          <img src={value.avatar} width="60"/>
+          <img src={value.avatar} width="60" />
         </td>
       </tr>
     )
   })
+
+  const handleInputField = (event) => {
+    updateUserForm({...userForm, [event.target.name] : event.target.value });
+  }
+
+  const createUserAccount = () => {
+    console.log(userForm);
+
+    const url = "https://reqres.in/api/users";
+   
+    axios.post(url, userForm)
+      .then( (response) => {
+        console.log(response.data);
+      })
+      .catch( (error) => {
+        alert(error.message);
+        console.log(error);
+      })
+  }
 
   return (
     <div>
@@ -58,6 +81,17 @@ const ApiPage = () => {
           {usersResult}
         </tbody>
       </table>
+
+      <h2>POST Method</h2>
+      <div>
+        <label>Enter your Name</label>
+        <input type='text' name='name' placeholder='Enter Name' onChange={handleInputField}/>
+      </div>
+      <div>
+        <label>Enter your Job</label>
+        <input type='text' name='job' placeholder='Enter Job' onChange={handleInputField}/>
+      </div>
+      <button onClick={() => createUserAccount()}>Submit</button>
     </div>
   );
 };
